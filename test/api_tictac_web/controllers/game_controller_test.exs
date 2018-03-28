785b703b50eaf4fe2089574cfb0fa76a30c33c38
Tam Dao <11000026+tamminhdao@users.noreply.github.com>
2018-03-28 17:08:54 -0500

    make_move sends back the right board

--- test/api_tictac_web/controllers/game_controller_test.exs
+++ test/api_tictac_web/controllers/game_controller_test.exs
@@ -15,4 +15,74 @@ defmodule Api.ApiTictacWeb.GameControllerTest do
 
     assert response == expect
   end
+
+  test "new_game/2 clears the board and starts a new game", %{conn: conn} do
+    response =
+      conn
+      |> get(game_path(conn, :new_game))
+      |> get(game_path(conn, :make_move, 1))
+      |> get(game_path(conn, :new_game))
+      |> json_response(200)
+
+    expect = %{
+      "Game Status" => "In Progress",
+      "Winner" => nil,
+      "Board" => ["empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty", "empty"]
+    }
+
+    assert response == expect
+  end
+
+  test "make_move/2 returns a board with players' moves, game status and winner status", %{conn: conn} do
+    response =
+      conn
+      |> get(game_path(conn, :new_game))
+      |> get(game_path(conn, :make_move, 1))
+      |> json_response(200)
+
+    expect = %{
+      "Game Status" => "In Progress",
+      "Winner" => nil,
+      "Board" => ["AI", "H", "empty", "empty", "empty", "empty", "empty", "empty", "empty"]
+    }
+
+    assert response == expect
+  end
+
+  test "make_move/2 returns status Game Over when there is a winner", %{conn: conn} do
+    response =
+      conn
+      |> get(game_path(conn, :new_game))
+      |> get(game_path(conn, :make_move, 3))
+      |> get(game_path(conn, :make_move, 5))
+      |> get(game_path(conn, :make_move, 8))
+      |> json_response(200)
+
+    expect = %{
+      "Game Status" => "Game Over",
+      "Winner" => "AI",
+      "Board" => ["AI", "AI", "AI", "H", "empty", "H", "empty", "empty", "H"]
+    }
+
+    assert response == expect
+  end
+
+  test "make_move/2 stops update the board if there already a winner", %{conn: conn} do
+    response =
+      conn
+      |> get(game_path(conn, :new_game))
+      |> get(game_path(conn, :make_move, 3))
+      |> get(game_path(conn, :make_move, 5))
+      |> get(game_path(conn, :make_move, 8))
+      |> get(game_path(conn, :make_move, 7))
+      |> json_response(200)
+
+    expect = %{
+      "Game Status" => "Game Over",
+      "Winner" => "AI",
+      "Board" => ["AI", "AI", "AI", "H", "empty", "H", "empty", "empty", "H"]
+    }
+
+    assert response == expect
+  end
 end
